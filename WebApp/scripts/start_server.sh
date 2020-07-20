118225d47de491ee697d083ec27130485ef2e107
Andreas Calvo Gomez <ndreasg@amazon.es>
2020-07-20 09:16:46 +0200

    make sure no node processes are running before app start

--- WebApp/scripts/start_server.sh
+++ WebApp/scripts/start_server.sh
@@ -2,6 +2,13 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+#make sure no Node processes are running
+service=node$
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))$
+then$
+killall node$
+fi$
+
 cd /home/ec2-user/node-website
 export PORT=80
 forever start app.js
